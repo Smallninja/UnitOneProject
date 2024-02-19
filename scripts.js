e2c5ccbe46dcaf1cb9bdfb8d7ea9f3a505902dfc
Smallninja <Smallninja.jay@gmail.com>
2024-02-18 20:17:03 -0600

    Created Greatness

--- scripts.js
+++ scripts.js
@@ -5,6 +5,14 @@ for (let i = 0; i < submitBtn.length; i++) {
     submitBtn[i].addEventListener('click', function(event) {
         firstName = document.getElementById("firstName").value;
         lastName = document.getElementById("lastName").value;
-        displayName.textContent = `Hello, ${firstName} ${lastName}!`;
+
+        let newLink = document.createElement('a');
+        newLink.href = 'https://github.com/Smallninja/UnitOneProject.git';
+        newLink.textContent = "Click here to go on an adventure!"
+        newLink.classList.add('btn', 'btn-white');
+        
+        displayName.textContent = `Hello, ${firstName} ${lastName}! `;
+        let referenceNode = document.getElementById('displayName');
+        referenceNode.parentNode.insertBefore(newLink, referenceNode.nextSibling);
     });
 }
